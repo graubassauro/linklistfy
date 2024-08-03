fcbdf0e910364ee1696cbc7672715eb6420fbb57
glaubert stransky <graubassauro@users.noreply.github.com>
2024-08-02 20:14:01 -0400

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,8 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
   {
     path: "",
-    pathMatch: "full",
-    redirectTo: "/:id",
+    component: HomeComponent,
   },
-
   {
     path: ":id",
     component: HomeComponent,
